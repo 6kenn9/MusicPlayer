bb832c446666269c3c7fe0bd35bd895fc456700c
DmytroBabich <dmitrobabic017@gmail.com>
2025-11-19 23:57:41 +0200

    Created Trackbar and controls

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -40,6 +40,7 @@
             this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.завантажитиПіснюToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mainTrackBar = new Guna.UI2.WinForms.Guna2TrackBar();
             this.panelSideMenu.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbAlbumArt)).BeginInit();
             this.guna2Panel1.SuspendLayout();
@@ -49,23 +50,22 @@
             // panelSideMenu
             // 
             this.panelSideMenu.BackColor = System.Drawing.Color.DarkViolet;
+            this.panelSideMenu.Controls.Add(this.mainTrackBar);
             this.panelSideMenu.Controls.Add(this.lblTrackTitle);
             this.panelSideMenu.Controls.Add(this.pbAlbumArt);
             this.panelSideMenu.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panelSideMenu.Location = new System.Drawing.Point(0, 40);
-            this.panelSideMenu.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panelSideMenu.Location = new System.Drawing.Point(0, 30);
             this.panelSideMenu.Name = "panelSideMenu";
-            this.panelSideMenu.Size = new System.Drawing.Size(592, 737);
+            this.panelSideMenu.Size = new System.Drawing.Size(395, 467);
             this.panelSideMenu.TabIndex = 0;
             // 
             // lblTrackTitle
             // 
             this.lblTrackTitle.BackColor = System.Drawing.Color.Transparent;
             this.lblTrackTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.lblTrackTitle.Location = new System.Drawing.Point(204, 286);
-            this.lblTrackTitle.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.lblTrackTitle.Location = new System.Drawing.Point(137, 191);
             this.lblTrackTitle.Name = "lblTrackTitle";
-            this.lblTrackTitle.Size = new System.Drawing.Size(172, 39);
+            this.lblTrackTitle.Size = new System.Drawing.Size(113, 27);
             this.lblTrackTitle.TabIndex = 1;
             this.lblTrackTitle.Text = "Назва пісні";
             // 
@@ -74,20 +74,19 @@
             this.pbAlbumArt.Image = ((System.Drawing.Image)(resources.GetObject("pbAlbumArt.Image")));
             this.pbAlbumArt.ImageRotate = 0F;
             this.pbAlbumArt.InitialImage = ((System.Drawing.Image)(resources.GetObject("pbAlbumArt.InitialImage")));
-            this.pbAlbumArt.Location = new System.Drawing.Point(177, 19);
-            this.pbAlbumArt.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pbAlbumArt.Location = new System.Drawing.Point(118, 29);
             this.pbAlbumArt.Name = "pbAlbumArt";
-            this.pbAlbumArt.Size = new System.Drawing.Size(225, 234);
+            this.pbAlbumArt.Size = new System.Drawing.Size(150, 150);
             this.pbAlbumArt.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pbAlbumArt.TabIndex = 0;
             this.pbAlbumArt.TabStop = false;
             // 
             // trackBarVolume
             // 
-            this.trackBarVolume.Location = new System.Drawing.Point(136, 141);
-            this.trackBarVolume.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.trackBarVolume.Location = new System.Drawing.Point(91, 90);
             this.trackBarVolume.Name = "trackBarVolume";
-            this.trackBarVolume.Size = new System.Drawing.Size(450, 36);
+            this.trackBarVolume.Size = new System.Drawing.Size(300, 23);
+            this.trackBarVolume.Style = Guna.UI2.WinForms.Enums.TrackBarStyle.Metro;
             this.trackBarVolume.TabIndex = 2;
             this.trackBarVolume.ThumbColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(113)))), ((int)(((byte)(255)))));
             // 
@@ -100,11 +99,10 @@
             this.btnPrev.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(113)))), ((int)(((byte)(255)))));
             this.btnPrev.Font = new System.Drawing.Font("Segoe MDL2 Assets", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnPrev.ForeColor = System.Drawing.Color.White;
-            this.btnPrev.Location = new System.Drawing.Point(114, 5);
-            this.btnPrev.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnPrev.Location = new System.Drawing.Point(76, 3);
             this.btnPrev.Name = "btnPrev";
             this.btnPrev.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
-            this.btnPrev.Size = new System.Drawing.Size(112, 109);
+            this.btnPrev.Size = new System.Drawing.Size(75, 70);
             this.btnPrev.TabIndex = 3;
             this.btnPrev.Text = "";
             // 
@@ -117,11 +115,10 @@
             this.btnPlay.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(113)))), ((int)(((byte)(255)))));
             this.btnPlay.Font = new System.Drawing.Font("Segoe MDL2 Assets", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnPlay.ForeColor = System.Drawing.Color.White;
-            this.btnPlay.Location = new System.Drawing.Point(288, 5);
-            this.btnPlay.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnPlay.Location = new System.Drawing.Point(192, 3);
             this.btnPlay.Name = "btnPlay";
             this.btnPlay.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
-            this.btnPlay.Size = new System.Drawing.Size(112, 109);
+            this.btnPlay.Size = new System.Drawing.Size(75, 70);
             this.btnPlay.TabIndex = 4;
             this.btnPlay.Text = "";
             this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
@@ -135,11 +132,10 @@
             this.btnNext.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(113)))), ((int)(((byte)(255)))));
             this.btnNext.Font = new System.Drawing.Font("Segoe MDL2 Assets", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnNext.ForeColor = System.Drawing.Color.White;
-            this.btnNext.Location = new System.Drawing.Point(459, 5);
-            this.btnNext.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnNext.Location = new System.Drawing.Point(306, 3);
             this.btnNext.Name = "btnNext";
             this.btnNext.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
-            this.btnNext.Size = new System.Drawing.Size(112, 109);
+            this.btnNext.Size = new System.Drawing.Size(75, 70);
             this.btnNext.TabIndex = 5;
             this.btnNext.Text = "";
             // 
@@ -152,11 +148,10 @@
             this.guna2CircleButton1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(113)))), ((int)(((byte)(255)))));
             this.guna2CircleButton1.Font = new System.Drawing.Font("Segoe MDL2 Assets", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guna2CircleButton1.ForeColor = System.Drawing.Color.White;
-            this.guna2CircleButton1.Location = new System.Drawing.Point(33, 119);
-            this.guna2CircleButton1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.guna2CircleButton1.Location = new System.Drawing.Point(22, 76);
             this.guna2CircleButton1.Name = "guna2CircleButton1";
             this.guna2CircleButton1.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
-            this.guna2CircleButton1.Size = new System.Drawing.Size(72, 72);
+            this.guna2CircleButton1.Size = new System.Drawing.Size(48, 46);
             this.guna2CircleButton1.TabIndex = 6;
             this.guna2CircleButton1.Text = "";
             // 
@@ -169,42 +164,52 @@
             this.guna2Panel1.Controls.Add(this.btnPlay);
             this.guna2Panel1.Controls.Add(this.trackBarVolume);
             this.guna2Panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.guna2Panel1.Location = new System.Drawing.Point(592, 575);
-            this.guna2Panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.guna2Panel1.Location = new System.Drawing.Point(395, 368);
             this.guna2Panel1.Name = "guna2Panel1";
-            this.guna2Panel1.Size = new System.Drawing.Size(701, 202);
+            this.guna2Panel1.Size = new System.Drawing.Size(467, 129);
             this.guna2Panel1.TabIndex = 2;
             this.guna2Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.guna2Panel1_Paint);
             // 
             // menuStrip1
             // 
-            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.завантажитиПіснюToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1293, 40);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 1, 0, 1);
+            this.menuStrip1.Size = new System.Drawing.Size(862, 30);
             this.menuStrip1.TabIndex = 3;
             this.menuStrip1.Text = "menuStrip1";
             // 
             // завантажитиПіснюToolStripMenuItem
             // 
             this.завантажитиПіснюToolStripMenuItem.Name = "завантажитиПіснюToolStripMenuItem";
-            this.завантажитиПіснюToolStripMenuItem.Size = new System.Drawing.Size(249, 38);
+            this.завантажитиПіснюToolStripMenuItem.Size = new System.Drawing.Size(158, 28);
             this.завантажитиПіснюToolStripMenuItem.Text = "Завантажити пісню";
             this.завантажитиПіснюToolStripMenuItem.Click += new System.EventHandler(this.завантажитиПіснюToolStripMenuItem_Click);
             // 
+            // mainTrackBar
+            // 
+            this.mainTrackBar.BackColor = System.Drawing.Color.DarkOrchid;
+            this.mainTrackBar.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.mainTrackBar.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.mainTrackBar.Location = new System.Drawing.Point(0, 444);
+            this.mainTrackBar.Name = "mainTrackBar";
+            this.mainTrackBar.Size = new System.Drawing.Size(395, 23);
+            this.mainTrackBar.TabIndex = 2;
+            this.mainTrackBar.ThumbColor = System.Drawing.Color.Black;
+            this.mainTrackBar.Scroll += new System.Windows.Forms.ScrollEventHandler(this.guna2TrackBar1_Scroll);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1293, 777);
+            this.ClientSize = new System.Drawing.Size(862, 497);
             this.Controls.Add(this.guna2Panel1);
             this.Controls.Add(this.panelSideMenu);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "BetterSpotify";
             this.panelSideMenu.ResumeLayout(false);
@@ -231,6 +236,7 @@
         private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem завантажитиПіснюToolStripMenuItem;
+        private Guna.UI2.WinForms.Guna2TrackBar mainTrackBar;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -45,5 +45,10 @@ namespace MusicPlayer
                     Console.WriteLine(f);
             }
         }
+
+        private void guna2TrackBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+
+        }
     }
 }
