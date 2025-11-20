dd6f1b9ba6b75280a28ac510f5439d59006eddae
ArtemKovtoniuk <testc8882@gmail.com>
2025-11-20 10:18:54 +0200

    commit

--- AudioService.cs
+++ AudioService.cs
@@ -7,35 +7,5 @@ using NAudio.Wave;
 
 namespace MusicPlayer
 {
-    class AudioService
-    {
-        private WaveOutEvent _waveOut;
-        private AudioFileReader _audioFile;
 
-        public AudioService()
-        {
-            _waveOut = new WaveOutEvent();
-        }
-        public void SetVolume(float volume)
-        {
-            if (_waveOut == null)
-                return;
-
-            if (volume < 0f) volume = 0f;
-            if (volume > 1f) volume = 1f;
-
-            _waveOut.Volume = volume;
-        }
-        public void Load(string path)
-        {
-            _audioFile = new AudioFileReader(path);
-            _waveOut.Init(_audioFile);
-        }
-        public void Play()
-        {
-            if (_audioFile == null) return;
-
-            _waveOut.Play();
-        }
-    }
 }
